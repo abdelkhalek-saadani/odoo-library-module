f9b35a60e3418e031af32c2da91325d36b6e0f44
abdelkhaleksaadani <abdelkhaleksaadani@gmail.com>
2024-03-23 09:02:27 +0100

    second commit

--- library (copy)/models/author.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api, _
-
-class LibraryAuthor(models.Model):
-    _name = "library.author"
-    _description = "for author records"
-
-    name = fields.Char(string = "Name", required = True, tracking=True)
-    date_of_birth = fields.Date(string="Date of Birth")
-    birthplace = fields.Char(string="Birthplace")
--- library (copy)/models/category.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-class LibraryCategory(models.Model):
-    _name="library.category"
-    _description="book categories"
-
-    name=fields.Char(string="Category name", required="true")
-    book_ids = fields.One2many("library.book","category_id", string="Books",)
